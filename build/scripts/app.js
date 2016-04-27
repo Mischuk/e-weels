6f6e8889125b64bb1d63304bd9825e3f8358ba75
Alexander Mischuk <mischuk.alexander@gmail.com>
2016-04-27 19:27:41 +0300

    Blocks: 1, 2

--- build/scripts/app.js
+++ build/scripts/app.js
@@ -2,4 +2,65 @@ $(document).ready(function() {
   $('a[href="#"]').click(function(e){
     e.preventDefault();
   });
+
+  $('#nav').onePageNav();
+
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 33) {
+        $('header').addClass('small');
+    } else {
+        $('header').removeClass('small');
+    }
+  });
+  $(".logo").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, 0);
+    return false;
+  });
+
+
+
+  var a = 0;
+  for (; a < 15; a += 1) {
+      setTimeout(function b() {
+          var a = Math.random() * 1e3 + 5e3,
+              c = $("<div />", {
+                  "class": "smoke",
+                  css: {
+                      opacity: 0,
+                      left: Math.random() * 200 + 80
+                  }
+              });
+          $(c).appendTo("#viewport");
+          $.when($(c).animate({
+              opacity: 1
+          }, {
+              duration: a / 4,
+              easing: "linear",
+              queue: false,
+              complete: function() {
+                  $(c).animate({
+                      opacity: 0
+                  }, {
+                      duration: a / 3,
+                      easing: "linear",
+                      queue: false
+                  })
+              }
+          }), $(c).animate({
+              bottom: $("#viewport").height()
+          }, {
+              duration: a,
+              easing: "linear",
+              queue: false
+          })).then(function() {
+              $(c).remove();
+              b()
+          })
+      }, Math.random() * 3e3)
+  };
+  //$(function(){
+  //  if(!$.browser.msie){
+  //
+  //  }else{
+  //  "use strict";var a=0;for(;a<15;a+=1){setTimeout(function b(){var a=Math.random()*1e3+5e3,c=$("<div />",{"class":"smoke",css:{left:Math.random()*200+80}});$(c).appendTo("#viewport");$.when($(c).animate({},{duration:a/4,easing:"linear",queue:false,complete:function(){$(c).animate({},{duration:a/3,easing:"linear",queue:false})}}),$(c).animate({bottom:$("#viewport").height()},{duration:a,easing:"linear",queue:false})).then(function(){$(c).remove();b()})},Math.random()*3e3)}}}());
 });
--- gulpfile.js
+++ gulpfile.js
@@ -70,30 +70,12 @@ gulp.task('app:styl', function() {
     .pipe(connect.reload());
 });
 
-// Fast styles
-gulp.task('styles:styl', function() {
-  var onError = function(err) {
-    notify.onError({
-      title:    "Styles",
-      subtitle: "Failure!",
-      message:  "Error: <%= error.message %>"
-    })(err);
-    this.emit('end');
-  };
-  return gulp.src("development/styles/styles.styl")
-    .pipe(plumber({ errorHandler: onError }))
-    .pipe(sourcemaps.init())
-    .pipe(stylus())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest("build/styles"))
-    .pipe(connect.reload());
-});
+
 
 gulp.task('watch', function () {
-  gulp.watch("development/styles/includes/*.styl", ['appstyl']);
-  gulp.watch("development/styles/modules/*.styl", ['appstyl']);
-  gulp.watch("development/styles/global.styl", ['app:styl','styles:styl']);
-  gulp.watch("development/styles/styles.styl", ['styles:styl']);
+  gulp.watch("development/styles/includes/*.styl", ['app:styl']);
+  gulp.watch("development/styles/modules/*.styl", ['app:styl']);
+  gulp.watch("development/styles/global.styl", ['app:styl']);
   gulp.watch("development/index.jade", ['jade']);
   gulp.watch("development/templates/*.jade", ['jade']);
   gulp.watch("development/images/**/*.jpg", ['jpg']);
@@ -104,4 +86,4 @@ gulp.task('watch', function () {
 });
 
 // Watching project files
-gulp.task('default', ['jade', 'app:styl', 'styles:styl', 'js', 'png', 'jpg', 'connect', 'watch']);
+gulp.task('default', ['jade', 'app:styl', 'js', 'png', 'jpg', 'connect', 'watch']);
